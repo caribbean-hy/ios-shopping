e1d4d239119468a9374bbfaecb3acef166a7ce04
Winnie Wu <weiwinnie.wu@gmail.com>
2013-07-12 14:13:53 -0700

    first commit

---
+++ ParseStarterProject/Course.h
@@ -0,0 +1,13 @@
+//
+//  Course.h
+//  ParseStarterProject
+//
+//  Created by Winnie Wu on 7/12/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Course : NSObject
+
+@end
---
+++ ParseStarterProject/Course.m
@@ -0,0 +1,29 @@
+//
+//  Course.m
+//  ParseStarterProject
+//
+//  Created by Winnie Wu on 7/12/13.
+//
+//
+
+#import "Course.h"
+@interface Course ()
+
+@property (nonatomic) int cat_num;
+@property (nonatomic) BOOL term;
+@property (nonatomic, strong) NSString *field;
+@property (nonatomic) int number;
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSString *faculty;
+@property (nonatomic, strong) NSString *description;
+@property (nonatomic, strong) NSString *prerequisites;
+@property (nonatomic, strong) NSString *meetings;
+@property (nonatomic, strong) NSString *building;
+@property (nonatomic, strong) NSString *room;
+
+@end
+
+
+@implementation Course
+
+@end
